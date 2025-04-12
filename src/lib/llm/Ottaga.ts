f9009a6134d0d4599b4e4e85a2cb9ef5038b4616
Ethan Henrickosn <109119138+EthanHenrickson@users.noreply.github.com>
2025-04-11 20:57:20 -0500

    Update Ottaga LLM to have createChat function.

--- src/lib/llm/Ottaga.ts
+++ src/lib/llm/Ottaga.ts
@@ -5,6 +5,7 @@ import OpenAI from "openai";
 import type { LLMConfig, Message } from "$lib/types";
 import { BaseLLM } from './LLM';
 import { Helper } from './HelperLLM';
+import { ChatDatabase } from '$lib/db/chat';
 
 /**
  * A client for interacting with language learning models through the OpenAI API.
@@ -21,24 +22,54 @@ class OttagaLLM extends BaseLLM {
         super(OttagaConfig)
     }
 
+
+    /**
+     * Creates a chat in Database, generates system prompt, and then saves it oo database
+     */
+    CreateChat(userInfo?: { userID: string, pastSessionSummaries?: string[] }): string {
+        //Create a chat with userID if provided
+        let newChat = ChatDatabase.createChat(userInfo?.userID)
+
+        if (newChat.success) {
+            //Generate system prompt
+            const systemMessage = this.GenerateSystemPrompt(userInfo?.pastSessionSummaries)
+            ChatDatabase.addChatMessage(newChat.data, systemMessage)
+
+            //Return chatID
+            return newChat.data
+        } else {
+            throw Error("Failed to create Chat for Ottaga")
+        }
+    }
+
     /**
      * Generates a system prompt that includes past session summaries.
      * @param pastUserSessionSummaries - Array of previous session summaries to include in the prompt
      * @returns A Message object containing the generated system prompt
      */
-    private async AppendSessionSummariesTooSystemPrompt(pastUserSessionSummaries: string[] = []): Promise<Message> {
-        let returnPrompt = `
-            ${this.SystemPrompt} \n
-            Here is a summary of the max last ${pastUserSessionSummaries.length} sessions. \n`
-
-        pastUserSessionSummaries.forEach((summary, index) => {
-            returnPrompt += `<session ${index + 1}> ${summary} </session ${index + 1}>`
-        })
-
-        return {
+    private GenerateSystemPrompt(pastUserSessionSummaries?: string[]): Message {
+        //Return variable
+        let returnMessage: Message = {
             role: "system",
-            content: returnPrompt
+            content: ""
         }
+
+        if (pastUserSessionSummaries) {
+            //Generate new prompt and append past user summaries to it
+            let newPrompt = `${this.SystemPrompt} \n Here is a summary of the max last ${pastUserSessionSummaries.length} sessions. \n`
+
+            pastUserSessionSummaries.forEach((summary, index) => {
+                newPrompt += `<session ${index + 1}> ${summary} </session ${index + 1}>`
+            })
+
+            returnMessage.content = newPrompt
+            
+        } else {
+            //User basic system prompt
+            returnMessage.content = this.SystemPrompt
+        }
+
+        return returnMessage
     }
 
     /**
@@ -47,10 +78,9 @@ class OttagaLLM extends BaseLLM {
      * @returns A Promise resolving to the model's response as a Message
      * @throws Error if the model's response is undefined
      */
-    async SendMessage(pastMessages: Message[], newMessage: Message, pastUserSessionSummaries?: string[]): Promise<{ messageWasMalicious: boolean, data: Message }> {
+    async SendMessage(pastMessages: Message[], newMessage: Message): Promise<{ messageWasMalicious: boolean, data: Message }> {
         const isUserMessageMalicious = await Helper.isMessageMalicious(newMessage)
-        const systemPrompt = pastUserSessionSummaries ? await this.AppendSessionSummariesTooSystemPrompt(pastUserSessionSummaries) : this.SystemPromptAsMessage
-        const messages = [systemPrompt, ...pastMessages, newMessage]
+        const messages = [...pastMessages, newMessage]
 
         //Default message
         const responseMessage: Message = {
--- src/routes/(marketing)/chat/+page.server.ts
+++ src/routes/(marketing)/chat/+page.server.ts
@@ -1,16 +1,12 @@
 import type { PageServerLoad } from "./$types";
-import { ChatDatabase } from "$lib/db/chat";
+import { Ottaga } from "$lib/llm/Ottaga";
 
 export const load: PageServerLoad = async () => {
     //Create a new chat save the chat id
-    let newChatInstance = ChatDatabase.createChat()
+    let chatID = Ottaga.CreateChat()
 
-    if(newChatInstance.success){
-        return { 
-            chatID: newChatInstance.data 
-        }
-    } else {
-        throw Error("Couldn't create new chat")
+    return {
+        chatID: chatID
     }
 
 };
